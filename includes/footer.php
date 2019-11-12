3c7424266cb2464d10fbbb704785f8af4cb7bb08
59050083@kmitl.ac.th <59050083@kmitl.ac.th>
2019-11-12 12:59:48 +0700

    เพิ่ม linl ของ lib selectpicker

--- includes/footer.php
+++ includes/footer.php
@@ -21,6 +21,7 @@
 <script src="https://cdn.jsdelivr.net/npm/jquery@2.2.4/dist/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.4/dist/umd/popper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"></script>
+<script src="<?= ROOT ?>/assets/bootstrap-select/dist/js/bootstrap-select.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/jquery-match-height@0.7.2/dist/jquery.matchHeight.min.js"></script>
 <script src="<?= ROOT ?>/assets/js/main.js"></script>
 
