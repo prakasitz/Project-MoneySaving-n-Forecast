6e2da2c4cd874f8a6599070d0035ce6ff6f3c5e4
ploynantikan <57524302+ploynantikan@users.noreply.github.com>
2020-01-26 17:38:53 +0700

    ลบหน้าลงทะเบียนกับเข้าสู่ระบบออก

--- registration/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $con = mysqli_connect("localhost","root","","register");
-
-    //check connection
-    if(mysqli_connect_error()){
-        echo"Failed to connect to MySQL:".mysqli_connect_error();
-    }
-?>
