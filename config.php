183ba9bd5d0e5314df1c24e627f6b43c957d2ddb
prakasitz <prakasitbm@gmail.com>
2020-03-16 19:02:23 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,27 @@
 <?php
     define('ROOT', "http://" . $_SERVER['HTTP_HOST'] . '/zocute');
-// DB Config
-    Config::write( 'db.host', 'localhost' ); // if build to server, Please [ DON'T FORGET CHANGE HERE ] to Domain of Server.
-    Config::write( 'db.port', 'port_mysql' ); // check in xampp control panel Port(s)
-    Config::write( 'db.basename', 'your_db_name' ); // choose your database in mysql 
-    Config::write( 'db.user', 'your_username' ); //your username (Default: root) or if not exist let enter ""
-    Config::write( 'db.password', 'your_password' ); // your password (Default: "") or if not exist let enter ""
+    define( 'CLASS_PATH', realpath( dirname( __FILE__ ) . '/classes' ) );
+    require CLASS_PATH . '/Config.php';
 
-    date_default_timezone_set('asia/bangkok'); // set timezone
+// DB Config
+    try {
+
+        //ส่วนนี้คือส่วนสำหรับ ใช้งาน database
+        Config::write( 'db.host', 'localhost' ); // if build to server, Please [ DON'T FORGET CHANGE HERE ] to Domain of Server.
+        Config::write( 'db.port', 'your_port' ); // check in xampp control panel Port(s)
+        Config::write( 'db.basename', 'your_database' ); // choose your database in mysql 
+        Config::write( 'db.user', 'your_username' ); //your username (Default: root) or if not exist let enter ""
+        Config::write( 'db.password', 'your_password' ); // your password (Default: "") or if not exist let enter ""
+        // กรณีที่ รหัส error เป็น 2002 คือ port ผิด
+        // กรณีที่ รหัส error เป็น 1049 คือ ชื่อ database ไม่่มีใน dabast server
+        // กรณีที่ รหัส error เป็น 1045 คือ ชื่อ user ไม่ถูกต้อง หรือ pass ไม่ถูก
+        date_default_timezone_set('asia/bangkok'); // set timezone
+
+    } catch (\Throwable $th) {
+        echo "
+            <h1>Error Config!!</h1>
+            <h2>เนื่องจาก config database ไม่ถูกต้อง! <br><u>ลองใส่ ค่า config db ใหม่อีกครั้ง ถ้าใส่ถูกหรือมี db นั้นอยู่ จะไม่เห็น error</u></h2>
+        ";
+        echo $th;
+        die;
+    }
