876cbe15afefd2f2408db7592da9b014f27de3f8
sukanya141 <57525266+sukanya141@users.noreply.github.com>
2020-05-04 21:13:55 -0700

    Update register_1.php

--- login/register_1.php
+++ login/register_1.php
@@ -16,9 +16,9 @@
                     
                     <div class="row mb-3">
                         <h3 class="col-lg-6">ข้อมูลส่วนตัว</h3>
-                        <h3 class="col-lg-6">ข้อมูลการเงิน</h3>
+                       
                     </div>
-                    <form action="" method="post">
+                    <form action="register_db.php" method="get">
                         <div class="form-row">
                             <div class="col-lg-6 border-right pr-3" style="border-width:5px !important">
 
@@ -26,13 +26,13 @@
                                     <div class="col-lg-6">
                                         <div class="form-group">
                                             <label>Name</label>
-                                            <input type="email" class="form-control" placeholder="Name">
+                                            <input type="text" class="form-control" placeholder="Name" name="name">
                                         </div>
                                     </div>
                                     <div class="col-lg-6">
                                         <div class="form-group">
                                             <label>Surename</label>
-                                            <input type="email" class="form-control" placeholder="Surename">
+                                            <input type="text" class="form-control" placeholder="Surename" name="surename">
                                         </div>
                                     </div>
                                 </div>
@@ -42,13 +42,13 @@
                                     <div class="row" style="padding: 7px 0px 7px 0px;">
                                         <div class="col-md-6 col-lg-6">
                                             <div class="custom-control custom-radio">
-                                                <input type="radio" id="gMale" name="valGender" class="custom-control-input">
+                                                <input type="radio" id="gMale" name="valGender1" class="custom-control-input">
                                                 <label class="custom-control-label d-block" for="gMale">ชาย</label>
                                             </div>
                                         </div>
                                         <div class="col-md-6 col-lg-6">
                                             <div class="custom-control custom-radio">
-                                                <input type="radio" id="gFemale" name="valGender" class="custom-control-input">
+                                                <input type="radio" id="gFemale" name="valGender2" class="custom-control-input">
                                                 <label class="custom-control-label d-block" for="gFemale">หญิง</label>
                                             </div>
                                         </div>
@@ -57,33 +57,40 @@
 
                                 <div class="form-group">
                                     <label>วันเกิด</label>
-                                    <input type="date" min="1900-01-02" max="2100-01-01" class="form-control">
+                                    <input type="date" min="1900-01-02" max="2100-01-01" class="form-control" name="date">
                                 </div>
 
                                 <div class="form-group">
                                     <label>User Name</label>
-                                    <input type="email" class="form-control" placeholder="User Name">
+                                    <input type="test" class="form-control" placeholder="User Name" name="username">
                                 </div>
 
                                 <div class="form-group">
                                     <label>Email</label>
-                                    <input type="email" class="form-control" placeholder="Email">
+                                    <input type="email" class="form-control" placeholder="Email" name="email">
                                 </div>
 
                                 <div class="row">
                                     <div class="col-lg-6">
                                         <div class="form-group">
                                             <label>Password</label>
-                                            <input type="password" class="form-control" placeholder="Password">
+                                            <input type="password" class="form-control" placeholder="Password" name="pws">
                                         </div>
                                     </div>
                                     <div class="col-lg-6">
                                         <div class="form-group">
                                             <label>Re-assword</label>
-                                            <input type="email" class="form-control" placeholder="Re-password">
+                                            <input type="password" class="form-control" placeholder="Re-password">
                                         </div>
                                     </div>
+                                    
                                 </div>
+                                <div class="row form-group">
+                                        <div class="col-lg-8">
+                                            <button class="btn btn-info btn-block" type="submit">บันทึก</button>
+                                        </div>
+                                    </div>
+
                                 
                             </div>
                             <!-- section 2 -->
