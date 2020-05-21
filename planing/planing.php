f132ce79063454ef4812b280b6f99e2d6aaee6b2
prakasitz <prakasitbm@gmail.com>
2020-05-22 04:26:26 +0700

    แก้โค้ด insert planingใหม่ทั้งหมด

--- planing/planing.php
+++ planing/planing.php
@@ -26,125 +26,142 @@
         </div>
     </div>
 
-                <div class="card-body card-block">
-                    <form action="planing_db.php" id="form-record-income" method="GET" class="form-horizontal">
-                    <div class="col">
-                    <div class="col-lg-12">
-                        <div class="card">
-                            <div class="card-header row">
+    <div class="card-body card-block">
+        <div class="col">
+            <div class="col-lg-12">
+                <div class="card">
+                    <form action="planing_db.php" id="form-record-income" method="GET" >
+                    <!-- start form -->
+                        <div class="card-header">
+                            <div class="row">
                                 <label class="col-sm-3 col-lg-3 col-form-label font-weight-bold">วางแผนเป้าหมายเดือน</label>
                                 <div class="col-sm-5 col-lg-5">
-                                    <select class="selectpicker form-control"name = "m" id="selectMonthly">
-                            <option selected>เลือกเดือน</option>
-                            <option value="1" >มกราคม</option>
-                            <option value="2">กุมภาพันธ์</option>
-                            <option value="3">มีนาคม</option>
-                            <option value="4">เมษายน</option>
-                            <option value="5">พฤษภาคม</option>
-                            <option value="6">มิถุนายน</option>
-                            <option value="7">กรกฎาคม</option>
-                            <option value="8">สิงหาคม</option>
-                            <option value="9">กันยนยน</option>
-                            <option value="10">ตุลาคม</option>
-                            <option value="11">พฤศจิกายน</option>
-                            <option value="12">ธันวาคม</option>
-                        </select>
-                    </div>
-                </div>
-                    <div class="list-group">
-                        <div class="row form-group">
-                            
-                            <div class="input-group col-lg-6 col-sm-6">
-                                <div class="input-group-addon"><i class="fa fa-save"></i></div>
-                                <label class="col col-form-label text-left">การออม&การลงทุน</label>
-                            </div>
-                            <div class=" col-lg-2 col-sm-2">
-                                <input type="text" id="input-note" name="savings" class="form-control"
-                                    placeholder="">
+                                    <select class="selectpicker form-control" name="m" id="selectMonthly">
+                                        <option selected>เลือกเดือน</option>
+                                        <option value="1" >มกราคม</option>
+                                        <option value="2">กุมภาพันธ์</option>
+                                        <option value="3">มีนาคม</option>
+                                        <option value="4">เมษายน</option>
+                                        <option value="5">พฤษภาคม</option>
+                                        <option value="6">มิถุนายน</option>
+                                        <option value="7">กรกฎาคม</option>
+                                        <option value="8">สิงหาคม</option>
+                                        <option value="9">กันยนยน</option>
+                                        <option value="10">ตุลาคม</option>
+                                        <option value="11">พฤศจิกายน</option>
+                                        <option value="12">ธันวาคม</option>
+                                    </select>
+                                </div>
                             </div>
                         </div>
-                       
-                        <div class="row form-group">
-                            <div class="input-group col-lg-6 col-sm-6">
-                                <div class="input-group-addon"><i class="fa fa-barcode"></i></div>
-                                <label class="col col-form-label text-left">สาธารณูปโภค(ค่าน้ำ ค่าไฟ ค่าโทรศัพท์ บิลอื่นๆ)</label>
-                            </div>
-                            <div class=" col-lg-2 col-sm-2">
-                                <input type="text" id="input-note" name="bill" class="form-control"
-                                    placeholder="">
-                            </div>
-                        </div>
-                        <div class="row form-group">
-                            <div class="input-group col-lg-6 col-sm-6">
-                                <div class="input-group-addon"><i class="fa fa-group (alias)"></i></div>
-                                <label class="col col-form-label text-left">ครอบครัว&ส่วนตัว</label>
 
-                            </div>
-                            </div>
-                        <ul class="list">
-                        <div class="row form-group">
-                            <div class="input-group col-lg-6 col-sm-6">
-                                <label class="col col-form-label text-left pl-5">ครอบครัว</label>
+                        <div class="card-body list-group">
 
-                            </div>
-                            <div class=" col-lg-2 col-sm-2">
-                                <input type="text" id="input-note" name="fami" class="form-control"
-                                    placeholder="">
-                            </div>
-                            </div>
                             <div class="row form-group">
-                            <div class="input-group col-lg-6 col-sm-6">
-                                <label class="col col-form-label text-left pl-5">ส่วนตัว</label>
 
+                                <div class="input-group col-lg-6 col-sm-6">
+                                    <div class="input-group-addon"><i class="fa fa-save"></i></div>
+                                    <label class="col col-form-label text-left">การออม&การลงทุน</label>
+                                </div>
+                                <div class=" col-lg-2 col-sm-2">
+                                    <input type="hidden" name="savings_id" value="1">
+                                    <input type="text" id="input-note" name="savings" class="form-control"
+                                        placeholder="">
+                                </div>
                             </div>
-                            <div class=" col-lg-2 col-sm-2">
-                                <input type="text" id="input-note" name="personal" class="form-control"
-                                    placeholder="">
-                            </div>
-                            </div>
-                        </ul>
-                        <div class="row form-group">
-                            <div class="input-group col-lg-6 col-sm-6">
-                                <div class="input-group-addon"><i class="fa fa-coffee"></i></div>
-                                <label class="col col-form-label text-left">สันทนาการ(ช้อปปิ้ง สังสรรค์ ท่องเที่ยว)</label>
-                            </div>
-                            <div class=" col-lg-2 col-sm-2">
-                                <input type="text" id="input-note" name="recreation" class="form-control"
-                                    placeholder="">
-                            </div>
-                        </div>     
-                        <div class="row form-group">
-                            <div class="input-group col-lg-6 col-sm-6">
-                                <div class="input-group-addon"><i class="fa fa-credit-card"></i></div>
-                                <label class="col col-form-label text-left">หนี้สิน</label>
 
+                            <div class="row form-group">
+                                <div class="input-group col-lg-6 col-sm-6">
+                                    <div class="input-group-addon"><i class="fa fa-barcode"></i></div>
+                                    <label class="col col-form-label text-left">สาธารณูปโภค(ค่าน้ำ ค่าไฟ ค่าโทรศัพท์
+                                        บิลอื่นๆ)</label>
+                                </div>
+                                <div class=" col-lg-2 col-sm-2">
+                                    <input type="hidden" name="bill_id" value="2">
+                                    <input type="text" id="input-note" name="bill" class="form-control" placeholder="">
+                                </div>
                             </div>
-                            <div class=" col-lg-2 col-sm-2">
-                                <input type="text" id="input-note" name="debt" class="form-control"
-                                    placeholder="">
+
+                            <div class="row form-group">
+                                <div class="input-group col-lg-6 col-sm-6">
+                                    <div class="input-group-addon"><i class="fa fa-group (alias)"></i></div>
+                                    <label class="col col-form-label text-left">ครอบครัว&ส่วนตัว</label>
+                                </div>
                             </div>
+
+                            <ul class="list">
+                                <div class="row form-group">
+                                    <div class="input-group col-lg-6 col-sm-6">
+                                        <label class="col col-form-label text-left pl-5">ครอบครัว</label>
+                                    </div>
+                                    <div class=" col-lg-2 col-sm-2">
+                                        <input type="hidden" name="fami_id" value="3">
+                                        <input type="text" id="input-note" name="fami" class="form-control"
+                                            placeholder="">
+                                    </div>
+                                </div>
+                                <div class="row form-group">
+                                    <div class="input-group col-lg-6 col-sm-6">
+                                        <label class="col col-form-label text-left pl-5">ส่วนตัว</label>
+                                    </div>
+                                    <div class=" col-lg-2 col-sm-2">
+                                        <input type="hidden" name="personal_id" value="4">
+                                        <input type="text" id="input-note" name="personal" class="form-control"
+                                            placeholder="">
+                                    </div>
+                                </div>
+                            </ul>
+
+                            <div class="row form-group">
+                                <div class="input-group col-lg-6 col-sm-6">
+                                    <div class="input-group-addon"><i class="fa fa-coffee"></i></div>
+                                    <label class="col col-form-label text-left">สันทนาการ(ช้อปปิ้ง สังสรรค์
+                                        ท่องเที่ยว)</label>
+                                </div>
+                                <div class="col-lg-2 col-sm-2">
+                                    <input type="hidden" name="recreation_id" value="5">
+                                    <input type="text" id="input-note" name="recreation" class="form-control"
+                                        placeholder="">
+                                </div>
+                            </div>
+                            
+                            <div class="row form-group">
+                                <div class="input-group col-lg-6 col-sm-6">
+                                    <div class="input-group-addon"><i class="fa fa-credit-card"></i></div>
+                                    <label class="col col-form-label text-left">หนี้สิน</label>
+
+                                </div>
+                                <div class=" col-lg-2 col-sm-2">
+                                    <input type="hidden" name="debt_id" value="6">                                
+                                    <input type="text" id="input-note" name="debt" class="form-control" placeholder="">
+                                </div>
+                            </div>
+
+                            <div class="row form-group">
+                                <div class="input-group col-lg-6 col-sm-6">
+                                    <div class="input-group-addon"><i class="fa fa-circle"></i></div>
+                                    <label class="col col-form-label text-left">อื่นๆ</label>
+                                </div>
+                                <div class=" col-lg-2 col-sm-2">
+                                    <input type="hidden" name="other_id" value="7">                                  
+                                    <input type="text" id="input-note" name="other" class="form-control" placeholder="">
+                                </div>
+                            </div>
+
+                            <div class="row form-group">
+                                <div class="col-lg-8">
+                                    <button class="btn btn-info btn-block" type="submit">บันทึก</button>
+                                </div>
+                            </div>
+                            
                         </div>
-                        <div class="row form-group">
-                            <div class="input-group col-lg-6 col-sm-6">
-                                <div class="input-group-addon"><i class="fa fa-circle"></i></div>
-                                <label class="col col-form-label text-left">อื่นๆ</label>
-                            </div>
-                            <div class=" col-lg-2 col-sm-2">
-                                <input type="text" id="input-note" name="other" class="form-control"
-                                    placeholder="">
-                            </div>
-                        </div>
-                        <div class="row form-group">
-                            <div class="col-lg-8">
-                                <button class="btn btn-info btn-block" type="submit">บันทึก</button>
-                            </div>
-                        </div>
+                    <!-- end form -->
                     </form>
                 </div>
             </div>
         </div>
     </div>
-  
+
 </div>
 
 <?php
--- planing/planing_db.php
+++ planing/planing_db.php
@@ -1,35 +1,58 @@
 <?php
-    include '../config.php';
-    include '../classes/DB.php';
+include '../config.php';
+include '../classes/DB.php';
 
+session_start();
 
-    if(isset($_GET["savings"]) && isset($_GET["bill"]) && isset($_GET["fami"])&& isset($_GET["personal"])&& isset($_GET["recreation"]) && isset($_GET["debt"])&& isset($_GET["other"])&& isset($_GET["m"])) {
-      $savings = $_GET["savings"]; //การออมการลงทุน
-      $bill = $_GET["bill"]; //สาธารณุประโภค
-      $fami = $_GET["fami"]; //ครอบครัว
-      $personal = $_GET["personal"]; //ส่วนตัว
-      $recreation = $_GET["recreation"]; //สันทนาการ
-      $debt = $_GET["debt"]; //หนี้สิน
-      $other = $_GET["other"]; //อื่นๆ
-      $m = $_GET["m"]; //เดือน
-        $conn = DB::getInstance();
-        $sql = "INSERT INTO planning (savings,bill,fami,personal,recreation,debt,other,m)
-                VALUES ('$savings' , '$bill' ,'$fami','$personal','$recreation','$debt','$other','$m')
-             ";
-       // echo $sql; 
-        //echo $_GET["fami"]; 
-       // die;
-        $stmt = $conn->dbh->prepare( $sql );
-        $stmt->execute();
-        $conn = null;
-        //$result = $stmt->fetchAll( PDO::FETCH_ASSOC );
-        //print_r($result); เอาไว้ปริ้น ดูค่าที่ select มา
+if ( isset( 
+      $_GET["savings"],
+      $_GET["bill"], 
+      $_GET["fami"], 
+      $_GET["personal"], 
+      $_GET["recreation"], 
+      $_GET["debt"], 
+      $_GET["other"], 
+      $_GET["m"],
+      $_SESSION['users']
+    ) 
+    ) {
+    $plan_ids   = [
+                    $_GET["savings_id"]     => $_GET["savings"],
+                    $_GET["bill_id"]        => $_GET["bill"],
+                    $_GET["fami_id"]        => $_GET["fami"],
+                    $_GET["personal_id"]    => $_GET["personal"],
+                    $_GET["recreation_id"]  => $_GET["recreation"],
+                    $_GET["debt_id"]        => $_GET["debt"],
+                    $_GET["other_id"]       => $_GET["other"]
+    ];
 
-        if(isset($sql) != 0) {
+    $m          = $_GET["m"]; //เดือน
+    $user_id    = $_SESSION['users']['user_id'];
+
+    $conn = DB::getInstance();
+
+    #wtf
+    $sql  = " INSERT INTO `userplan` (`plan_id`, `uplan_value`, `month_id`, `user_id`)
+              VALUES ";
+
+    foreach ($plan_ids as $plan_id => $value) {
+      $sql .= "('$plan_id', '$value', '$m', '$user_id'),";
+    }
+
+    $sql = rtrim($sql,",");
+
+    // echo $sql;
+    // die;
+
+    $stmt = $conn->dbh->prepare( $sql );
+    $stmt->execute();
+    $conn = null;
+
+    if ( isset( $sql ) != 0 ) {
         echo "<script>alert('[Insert สำเร็จ]')</script>";
-        header("Refresh:0.5; url=http://localhost/zocute/index.php");
-        } else {
-            echo "<script>alert('ใส่ข้อมูลผิดพลาด')</script>";
-        }
-        
-      }
+        header( "Refresh:0.5; url=http://localhost/zocute/index.php" );
+    } else {
+        echo "<script>alert('ใส่ข้อมูลผิดพลาด')</script>";
+    }
+
+}
