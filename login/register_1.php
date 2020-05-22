ccf5b40d1712c8f9dd0cec8dbef10dd9fb6610da
sukanya141 <57525266+sukanya141@users.noreply.github.com>
2020-05-22 04:48:32 -0700

    regis

--- login/register_1.php
+++ login/register_1.php
@@ -18,7 +18,7 @@
                         <h3 class="col-lg-6">ข้อมูลส่วนตัว</h3>
                        
                     </div>
-                    <form action="register_db.php" method="get">
+                    <form action="register_db.php" method="POST">
                         <div class="form-row">
                             <div class="col-lg-6 border-right pr-3" style="border-width:5px !important">
 
@@ -26,13 +26,13 @@
                                     <div class="col-lg-6">
                                         <div class="form-group">
                                             <label>Name</label>
-                                            <input type="text" class="form-control" placeholder="Name" name="name">
+                                            <input type="text" class="form-control" placeholder="Name" name="user_fname">
                                         </div>
                                     </div>
                                     <div class="col-lg-6">
                                         <div class="form-group">
                                             <label>Surename</label>
-                                            <input type="text" class="form-control" placeholder="Surename" name="surename">
+                                            <input type="text" class="form-control" placeholder="Surename" name="user_sname">
                                         </div>
                                     </div>
                                 </div>
@@ -42,13 +42,13 @@
                                     <div class="row" style="padding: 7px 0px 7px 0px;">
                                         <div class="col-md-6 col-lg-6">
                                             <div class="custom-control custom-radio">
-                                                <input type="radio" id="gMale" name="valGender1" class="custom-control-input">
+                                                <input type="radio" id="gMale" name="user_gen" class="custom-control-input">
                                                 <label class="custom-control-label d-block" for="gMale">ชาย</label>
                                             </div>
                                         </div>
                                         <div class="col-md-6 col-lg-6">
                                             <div class="custom-control custom-radio">
-                                                <input type="radio" id="gFemale" name="valGender2" class="custom-control-input">
+                                                <input type="radio" id="gFemale" name="user_gen" class="custom-control-input">
                                                 <label class="custom-control-label d-block" for="gFemale">หญิง</label>
                                             </div>
                                         </div>
@@ -57,29 +57,29 @@
 
                                 <div class="form-group">
                                     <label>วันเกิด</label>
-                                    <input type="date" min="1900-01-02" max="2100-01-01" class="form-control" name="date">
+                                    <input type="date" min="1900-01-02" max="2100-01-01" class="form-control" name="user_bd">
                                 </div>
 
                                 <div class="form-group">
                                     <label>User Name</label>
-                                    <input type="test" class="form-control" placeholder="User Name" name="username">
+                                    <input type="text" class="form-control" placeholder="User Name" name="user_name">
                                 </div>
 
                                 <div class="form-group">
                                     <label>Email</label>
-                                    <input type="email" class="form-control" placeholder="Email" name="email">
+                                    <input type="email" class="form-control" placeholder="Email" name="user_email">
                                 </div>
 
                                 <div class="row">
                                     <div class="col-lg-6">
                                         <div class="form-group">
                                             <label>Password</label>
-                                            <input type="password" class="form-control" placeholder="Password" name="pws">
+                                            <input type="password" class="form-control" placeholder="Password" name="user_pass">
                                         </div>
                                     </div>
                                     <div class="col-lg-6">
                                         <div class="form-group">
-                                            <label>Re-assword</label>
+                                            <label>Re-password</label>
                                             <input type="password" class="form-control" placeholder="Re-password">
                                         </div>
                                     </div>
--- login/register_db.php
+++ login/register_db.php
@@ -3,20 +3,23 @@
     include '../classes/DB.php';
 
 
-    if(isset($_GET["name"]) && isset($_GET["surename"]) && isset($_GET["date"]) && isset($_GET["email"])&& isset($_GET["username"]) && isset($_GET["pws"])) {
-        $name = $_GET["name"];
-        $surename = $_GET["surename"];
-        $username = $_GET["username"];
-        $psw = $_GET["pws"];
-        $date = $_GET["date"];
-        $email = $_GET["email"];
+    if(isset($_POST["user_name"]) && isset($_POST["user_pass"]) && isset($_POST["user_email"])&& isset($_POST["user_fname"]) && isset($_GET["user_sname"])&& isset($_GET["user_gen"])&& isset($_GET["user_bd"])) {
+        $user_name = $_POST["user_name"];
+        $user_pass = $_POST["user_pass"];
+        $user_email = $_POST["user_email"];
+        $user_fname = $_POST["user_fname"];
+        $user_sname = $_POST["user_sname"];
+        $user_gen = $_POST["user_gen"];
+        $user_bd = $_POST["user_bd"];
         
 
         $conn = DB::getInstance();
-        $sql = "INSERT INTO useraccount (user_name,user_pass,user_email,user_fname,user_sname,user_bd)
-                VALUES ('$username' , '$psw' ,'$email', '$name' , '$surename', '$date')
+        $sql = "INSERT INTO usersaccount (user_name,user_pass,user_fname,user_sname,user_gen,user_bd)
+                VALUES (  '$user_name' ,'$user_pass', '$user_fname' , '$user_sname', '$user_gen','$user_bd')
                 ";
-        
+        //echo $sql;
+        print_r($_POST);
+        die;
 
         $stmt = $conn->dbh->prepare( $sql );
         $stmt->execute();
@@ -26,7 +29,7 @@
 
         if(isset($sql) != 0) {
         echo "<script>alert('[Insert สำเร็จ]')</script>";
-        header("Refresh:0.5; url=login.php");
+        header("Refresh:0.5; url=http://localhost/zocute/login/login.php");
         } else {
             echo "<script>alert('ใส่ข้อมูลผิดพลาด')</script>";
         }
