57b252e6d330b9dd9d32a333e6f1969ef9ab3149
ploynantikan <57524302+ploynantikan@users.noreply.github.com>
2020-05-29 15:27:53 +0700

    แบบฟอร์มหน้า login ใหม่
    ส่งต่อออยเชื่อมดาต้าเบส

---
+++ login/login_1.php
@@ -0,0 +1,50 @@
+<?php
+    $_GET['isShowNav'] = 0;
+    require_once '../config.php';
+    include_once '../includes/header.php';
+?>
+<body class="bg-dark small-device">
+
+    <div class="sufee-login d-flex align-content-center flex-wrap">
+        <div class="container">
+            <div class="login-content">
+                <div class="login-logo">
+                    <h1 class="text-light">เข้าสู่ระบบ</h1>
+                </div>
+                <div class="login-form">
+                    <form>
+                        <div class="form-group">
+                            <label>User Name</label>
+                            <input type="email" class="form-control" placeholder="User Name">
+                        </div>
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input type="password" class="form-control" placeholder="Password">
+                        </div>
+                        <div class="checkbox">
+                            <label>
+                                <input type="checkbox"> Remember Me
+                            </label>
+                            <label class="pull-right">
+                                <a href="#">Forgotten Password?</a>
+                            </label>
+                        </div>
+                        <button class="btn btn-info btn-block" type="submit">เข้าสู่ระบบ</button>
+                        <div class="register-link m-t-15 text-center">
+                            <p>Don't have account ? <a href="#"> Sign in</a></p>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@2.2.4/dist/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.4/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery-match-height@0.7.2/dist/jquery.matchHeight.min.js"></script>
+    <script src="assets/js/main.js"></script>
+
+
+
+</body>
