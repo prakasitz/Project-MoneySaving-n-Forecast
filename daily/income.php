2680a1bd390e011aed02882cab679890d16cc82f
ploynantikan <57524302+ploynantikan@users.noreply.github.com>
2020-06-01 14:43:31 +0700

    ลบคอมม่าออก

--- daily/income.php
+++ daily/income.php
@@ -66,51 +66,7 @@
                         <div class="row form-group">
                             <div class="input-group col-lg-12">
                                 <div class="input-group-addon"><strong>THB</strong></div>
-                                <input type="text" step="0.01" min="0" id="input-money-income" name="moneyIncome"  onkeyup="dokeyup(this);" onchange="dokeyup(this);" onkeypress="checknumber()" maxlength="12" class="form-control" placeholder="จำนวนเงิน">
-                                                                <script type="text/javaScript">
-                                                                        //เติม , (คอมมา)
-                                                                            function dokeyup( obj )
-                                                                            {
-                                                                                var key = event.keyCode;
-                                                                                    if( key != 37 & key != 39 & key != 110 )
-                                                                                    {
-                                                                                        var value = obj.value;
-                                                                                        var svals = value.split( "." ); //แยกทศนิยมออก
-                                                                                        var sval = svals[0]; //ตัวเลขจำนวนเต็ม
-                                                                                        var n = 0;
-                                                                                        var result = "";
-                                                                                        var c = "";
-                                                                                        for ( a = sval.length - 1; a >= 0 ; a-- )
-                                                                                        {
-                                                                                        c = sval.charAt(a);
-                                                                                        if ( c != ',' )
-                                                                                        {
-                                                                                            n++;
-                                                                                            if ( n == 4 )
-                                                                                            {
-                                                                                            result = "," + result;
-                                                                                            n = 1;
-                                                                                            };
-                                                                                            result = c + result;
-                                                                                        };
-                                                                                        };
-                                                                                        if ( svals[1] )
-                                                                                        {
-                                                                                        result = result + '.' + svals[1];
-                                                                                        };
-                                                                                        obj.value = result;
-                                                                                    };
-                                                                                    };
-                                                                                    //ให้ text รับค่าเป็นตัวเลขอย่างเดียว
-                                                                                    function checknumber()
-                                                                                    {
-                                                                                    key = event.keyCode;
-                                                                                    if ( key != 46 & ( key < 48 || key > 57 ) )
-                                                                                    {
-                                                                                        event.returnValue = false;
-                                                                                    };
-                                                                                    };
-                                                                                    </script> 
+                                <input type="text" step="0.01" min="0" id="input-money-income" name="moneyIncome"  onkeyup="dokeyup(this);" onchange="dokeyup(this);" onkeypress="checknumber()" maxlength="12" class="form-control" placeholder="จำนวนเงิน"> 
                             </div>
                         </div>
                         <div class="row form-group">
