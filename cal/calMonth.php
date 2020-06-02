40361a3ef6ced0bb622edf5be499b4b15d1e6aeb
prakasitz <prakasitbm@gmail.com>
2020-06-02 22:28:47 +0700

    แก้ไข calMonth ส่วนการแสดงผล

--- cal/calMonth.php
+++ cal/calMonth.php
@@ -29,20 +29,45 @@ if ( isset( $_SESSION['users'] ) ) {
 	//close connection
 	$conn = null;
 	
+	// DB::printArray($results); สำปรับปริ้นดูค่า array $results
 
 	$forecast=[];
+
 	function forecast(int $a, int $b,int $c) {
 		return  ($a*0.17)+($b*0.33)+($c*0.50);
   	}
 
-	for ($i = 0; $i < count($results)-3 ; $i++) {
-		array_push($forecast,forecast(($results[$i]["sumvalue"]),($results[$i+1]["sumvalue"]),($results[$i+2]["sumvalue"])));
-    }
-	      
 	
-	print_r($forecast);
+	for ($i = 0; $i < count($results)-2 ; $i++) {
+		//สร้าง array เปล่าสำหรับเก็บค่า month และ forecastval
+		$arr = [];
+		//สร้าง key month และเก็บ value เป็นเดือนที่ทำนาย
+		$arr['month'] = $results[$i+2]["month"]+1;
+		//สร้าง key forecastval และเก็บค่า ทำนาย
+		$arr['forecastval'] = forecast(($results[$i]["sumval"]),($results[$i+1]["sumval"]),($results[$i+2]["sumval"]));
+		// push เข้า array forecast ที่สร้างไว้นอก loop
+		array_push($forecast,$arr);
+	}
+	      
+	// ตอนนี้ array forecast เก็บข้อมูลลักษณะ array 2d โดยมี key คือ month และ forecast เป็น key ย่อย
+	// DB::printArray($forecast); สำปรับปริ้นดูค่า array $forecast
+
+	//loop นี้สำหรับ add ค่า forecastval ใน array forecast เข้าไป array results โดยเช็กจาก key month ว่าเท่ากันไหม
+	foreach ($results as $indRes => $res) {
+		foreach ($forecast as $indFrct => $frct) {
+			if($res['month'] == $frct['month']) {
+				echo $results[$indRes]['month'];
+				$results[$indRes]['forecastval'] = $frct['forecastval'];
+			}
+		}
+	}
+
+	//เหมือนเสร็จ loop จะเหลือข้อมูล forecast ตัวสุดท้าย เพราะเดือนมันจะมากกว่าเดือนใน result 
+	//เลยต้องใช้คำสั่ง array_pop เพื่อส่งออกข้อมูลสุดท้ายจาก array forecast แล้ว push เข้าไปใน result อีกครั้ง
+	array_push($results, array_pop($forecast));
 
 
+	// DB::printArray($results); สำปรับปริ้นดูค่า array $results
 
 
 
@@ -55,20 +80,28 @@ if ( isset( $_SESSION['users'] ) ) {
 	<td>YEAR</td>
 	<td>MONTH</td>
 	<td>SumValue</td>
+	<td>ทำนาย</td>
 	
 	</tr>";
 
-
+	$count = 0;
 	foreach ($results as $key => $value) {
 		echo "<tr>";
-		echo "<td>" . $value["num"] . "</td> ";
-		echo "<td>" . $value["user_id"] . "</td> ";
-		echo "<td>" . $value["year"] . "</td> ";
-		echo "<td>" . $value["month"] . "</td> ";
-		echo "<td>" . $value["sumval"] . "</td> ";
-	
+		echo "<td>" . strval($count += 1) . "</td> ";
+		echo (isset($value["user_id"]))? "<td>" . $value["user_id"] . "</td> "			:"<td></td>";
+		echo (isset($value["year"]))? "<td>" . $value["year"] . "</td> "				:"<td></td>";
+		echo (isset($value["month"]))? "<td>" . $value["month"] . "</td> "				:"<td></td>";
+		echo (isset($value["sumval"]))? "<td>" . $value["sumval"] . "</td> "			:"<td></td>";
+		echo (isset($value["forecastval"]))? "<td>" . $value["forecastval"] . "</td> "	:"<td></td>";
+		echo "</tr>";
 	}
 	echo "</table>";
+
+	//ปล.
+	// คำสั่ง isset ใช้ check ว่ามีค่านั้นๆจริงไหม ไม่งั้นจะ error ได้ เพราะข้อมูลจาก results ไม่มี key forecastval หรือข้อมูลจาก forecast ไม่มี user_id year sumval
+	// คำสั่ง isset จะส่งคืนค่าเป็น 0 ถ้าไม่มีค่านั้น เป็น 1 ถ้ามีค่านั้น
+	// คำสั่งที่เรียกว่า short-if จะมี syntax คือ (condition)? true : false;
+	// จากข้างบน จะเห็นว่า เมื่อ true จะปริ้น <td>value</td> แต่ถ้า false จะเป็น <td></td> เปล่าๆ
 	
 }
 
