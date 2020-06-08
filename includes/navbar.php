d4d036e703c3523f39f967ccbf9c53c117f7d249
ploynantikan <57524302+ploynantikan@users.noreply.github.com>
2020-06-08 22:58:12 +0700

    โลโก้หน้าเว็บ

--- includes/navbar.php
+++ includes/navbar.php
@@ -3,8 +3,8 @@
         <header id="header" class="header">
             <div class="top-left">
                 <div class="navbar-header">
-                    <a class="navbar-brand" href="<?= ROOT ?>/index.php"><img src="<?= ROOT ?>/images/logo.png" alt="Logo"></a>
-                    <a class="navbar-brand hidden" href="<?= ROOT ?>/index.php"><img src="<?= ROOT ?>/images/logo2.png" alt="Logo"></a>
+                    <a class="brand" href="<?= ROOT ?>/index.php"><img src="<?= ROOT ?>/images/kebtanglogo.png" alt="Logo"></a>
+                    <a class="navbar-brand hidden" href="<?= ROOT ?>/index.php"><img src="<?= ROOT ?>/images/money.png" alt="Logo"></a>
                     <a id="menuToggle" class="menutoggle"><i class="fa fa-bars"></i></a>
                 </div>
             </div>
