14a06f2f566bb42a0905be7b1bdb2caf4138e957
prakasitz <prakasitbm@gmail.com>
2020-06-14 19:20:36 +0700

    จัดการไฟล์ที่ไม่ได้ใช้ เพิ่ม Classes/Config&DB

---
+++ classes/DB.php
@@ -0,0 +1,33 @@
+<?php
+
+class DB {
+    public $dbh; // handle of the db connexion
+    private static $instance;
+
+    private function __construct() {
+        // building data source name from config
+        $dsn = 'mysql:host=' . Config::read('db.host') .
+               ';dbname='    . Config::read('db.basename') .
+               ';connect_timeout=15';
+        // getting DB user from config                
+        $user = Config::read('db.user');
+        // getting DB password from config                
+        $password = Config::read('db.password');
+
+        $this->dbh = new PDO($dsn, $user, $password);
+        $this->dbh->exec("set names utf8");
+    }
+
+    public static function getInstance() {
+        if (!isset(self::$instance))
+        {
+            $object = __CLASS__;
+            self::$instance = new $object;
+        }
+        return self::$instance;
+    }
+
+    // others global functions
+}
+
+?>
